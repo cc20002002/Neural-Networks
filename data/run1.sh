48ba1ac592361c4ff84bd377a95cd9201c660d03
Mingxuan Li <mingxuan.li.au@gmail.com>
2019-04-17 10:54:37 +1000

    commit code for table 2 test run

---
+++ data/run1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python3 Main_cc.py --config=./config/8987.json &
+python3 Main_cc.py --config=./config/batch_size_60000.json &
+python3 Main_cc.py --config=./config/no_weigh_decay.json &
+python3 Main_cc.py --config=./config/no_momentum.json  &
+python3 Main_cc.py --config=./config/dropout_05.json  &
