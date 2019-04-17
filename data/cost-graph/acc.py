73f38a7e2e8de82dda0c34d9ad1396810495c3ff
Mingxuan Li <mingxuan.li.au@gmail.com>
2019-04-17 16:39:41 +1000

    add acc graph

---
+++ data/cost-graph/acc.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+f = open('8987.json.csv', 'r')
+x = f.readlines()
+f.close()
+acc_8987 = list(map(float, x))
+
+f = open('batch_size_60000.json.csv', 'r')
+x = f.readlines()
+f.close()
+acc_batch_size_60000 = list(map(float, x))
+
+f = open('no_weigh_decay.json.csv', 'r')
+x = f.readlines()
+f.close()
+acc_no_weigh_decay = list(map(float, x))
+
+f = open('no_momentum.json.csv', 'r')
+x = f.readlines()
+f.close()
+acc_no_momentum = list(map(float, x))
+
+f = open('dropout_05.json.csv', 'r')
+x = f.readlines()
+f.close()
+acc_dropout_05 = list(map(float, x))
+
+figure = plt.figure()
+x = np.arange(200)
+plt.plot(acc_8987)
+plt.plot(acc_batch_size_60000)
+plt.plot(acc_no_weigh_decay)
+plt.plot(acc_no_momentum)
+plt.plot(acc_dropout_05)
+plt.ylabel("Accuracy(%)")
+plt.xlabel('Iteration')
+plt.legend(['best model', 'max batch', 'no weight decay', 'no momentum', 'dropout=0.5'], loc='lower right')
+plt.grid()
+plt.show()
+figure.set_size_inches(7,5)
+figure.savefig("acc.pdf", bbox_inches='tight')
+figure.savefig("acc.eps", bbox_inches='tight')
