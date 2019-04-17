031ea61da743cea64efe94ac777a5d252790eb86
Mingxuan Li <mingxuan.li.au@gmail.com>
2019-04-17 11:49:23 +1000

    add non parallel shell

---
+++ data/run2_non_parallel.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python3 Main_cc.py --config=./config/8987.json
+python3 Main_cc.py --config=./config/8995.json
+python3 Main_cc.py --config=./config/8972.json
+python3 Main_cc.py --config=./config/8975.json
+python3 Main_cc.py --config=./config/8932.json
