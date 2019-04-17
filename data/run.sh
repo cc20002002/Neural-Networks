3580c2fa48cc11de81b2c745267f1ed6838c5f54
Mingxuan Li <mingxuan.li.au@gmail.com>
2019-04-17 11:55:10 +1000

    update shell script

--- data/run.sh
+++ data/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-python3 Main_cc.py --config=./config/8987.json
-python3 Main_cc.py --config=./config/batch_size_60000.json
-python3 Main_cc.py --config=./config/no_weigh_decay.json
-python3 Main_cc.py --config=./config/no_momentum.json
-python3 Main_cc.py --config=./config/dropout_05.json
+python3 Main_cc.py --config=./config/8987.json &
+python3 Main_cc.py --config=./config/batch_size_60000.json &
+python3 Main_cc.py --config=./config/no_weigh_decay.json &
+python3 Main_cc.py --config=./config/no_momentum.json &
+python3 Main_cc.py --config=./config/dropout_05.json &
 
-python3 Main_cc.py --config=./config/8987.json
-python3 Main_cc.py --config=./config/8995.json
-python3 Main_cc.py --config=./config/8972.json
-python3 Main_cc.py --config=./config/8975.json
-python3 Main_cc.py --config=./config/8932.json
+python3 Main_cc.py --config=./config/8987.json &
+python3 Main_cc.py --config=./config/8995.json &
+python3 Main_cc.py --config=./config/8972.json &
+python3 Main_cc.py --config=./config/8975.json &
+python3 Main_cc.py --config=./config/8932.json &
